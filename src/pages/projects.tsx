21944685fb2dfbb68a79413236ae6799904d3717
Md Habibullah Al Hadi <hadicse@gmail.com>
2026-07-11 11:29:42 -0600

    1.0.0

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -20,7 +20,7 @@ export default function ProjectsPage() {
   const project    = useProjectsStore(s => s.projects.find(p => p.id === s.selectedId))
 
   return (
-    <div className="flex h-full gap-0 -m-6">
+    <div className="flex h-[calc(100%+3rem)] gap-0 -m-6">
       <ProjectList />
       <div className="flex-1 min-w-0">
         {selectedId && project ? <GanttView project={project} /> : <EmptyState />}
